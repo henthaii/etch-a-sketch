b54ad7a95eaea8534d4f0550d77d29f3e1c36ed6
henthaii <200550625+henthaii@users.noreply.github.com>
2026-01-07 21:56:14 -0600

    started on working on setting up js to create divs

---
+++ etch-a-sketch.js
@@ -0,0 +1,15 @@
+let etch = document.querySelector(".container");
+
+const content = document.createElement("div");
+content.classList.add("content");
+content.textContent = "This is the glorious text-content!";
+
+/*
+etch.forEach(container => {
+
+    const boxOne = document.createElement("div");
+    const boxTwo = document.createElement("div");
+    const boxThree = document.createElement("div");
+    const boxFour = document.createElement("div");
+})*/
+
